dcfb52da0853f961b41db7437d5ce1f29197f9de
MichalKrajewski6000 <m.krajewski6000@gmail.com>
2023-04-23 08:36:37 +0200

    Lab 3: Routing i Chat - Routing z React Router - Chat Box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
-import Layout from './components/Layout';
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import SiderMenu from "./components/SiderMenu";
+import Container from "./components/Container";
+import About from "./components/About";
+
+import ChatBox from "./components/ChatBlock";
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <Router>
+        <SiderMenu />
+        <Routes>
+          <Route exact path="/home" Component={Container} />
+          <Route exact path="/about" Component={About} />
+          {/* <Route exact path="/contact" Component={Layout} /> */}
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </Router>
+      <ChatBox />
     </div>
   );
 }
