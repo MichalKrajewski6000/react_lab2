29801531c9d4697c20b1f299eb49dc6e8e86e28b
MichalKrajewski6000 <m.krajewski6000@gmail.com>
2023-05-12 13:50:47 +0200

    Login Page - created login Page - created Private Route component

--- src/App.js
+++ src/App.js
@@ -11,16 +11,36 @@ import Container from "./components/Container";
 import About from "./components/About";
 
 import ChatBox from "./components/ChatBlock";
+import LoginPage from "./components/LoginPage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+  const user = sessionStorage.getItem("uid");
+
   return (
     <div className="App">
       <Router>
         <SiderMenu />
         <Routes>
-          <Route exact path="/home" Component={Container} />
-          <Route exact path="/about" Component={About} />
-          {/* <Route exact path="/contact" Component={Layout} /> */}
+          <Route exact path="/login" Component={LoginPage} />
+          <Route
+            exact
+            path="/home"
+            element={
+              <PrivateRoute user={user}>
+                <Container />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            exact
+            path="/about"
+            element={
+              <PrivateRoute user={user}>
+                <About />
+              </PrivateRoute>
+            }
+          />
           <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
